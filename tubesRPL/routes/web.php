5f33eb2929eec40fb1ad586cbace6e49486bd079
dhialif39 <74357255+dhialif39@users.noreply.github.com>
2022-04-23 13:20:26 +0700

    Create web.php

---
+++ tubesRPL/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Kemah;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', [KemahController::class, 'index']);
+// Route::get('/page_perkemahan', [KemahController::class, 'page_perkemahan']);
+
+Route::get('/', [AuthController::class, 'index']);
+Route::get('/login', [AuthController::class,"loginView"]);
+Route::get('/register', [AuthController::class,"registerView"]);
+Route::post('/do-login', [AuthController::class,"doLogin"]);
+Route::post('/do-register', [AuthController::class,"doRegister"]);
+Route::get('/logout', [AuthController::class,"logout"]);
